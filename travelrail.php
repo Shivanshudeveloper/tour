fcffd2858a07978e5e033c878075960b48c4bff7
Shivanshu981 <developer.shivanshu@gmail.com>
2021-01-28 08:41:20 -0800

    Tours and Travel

--- travelrail.php
+++ travelrail.php
@@ -23,6 +23,8 @@
   <link rel="stylesheet" href="css/flaticon.css">
   <link rel="stylesheet" href="css/style.css">
   <script src="https://kit.fontawesome.com/2915bda088.js" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
   <style>
     .avatar {
         vertical-align: middle;
@@ -31,6 +33,8 @@
         border-radius: 50%;
     }
   </style>
+  <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/js/splide.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/css/splide.min.css">
 </head>
 <body>
  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
@@ -69,7 +73,7 @@
       <div class="col-md-9 ftco-animate pb-5 text-center">
        <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>Travel <i class="fa fa-chevron-right"></i></span></p>
        <h1 class="mb-0 bread">
-        Travel Tickets Airways
+        Travel Tickets Railways
        </h1>
      </div>
    </div>
@@ -81,19 +85,19 @@
         <div class="card border-0">
             <div class="card-body">
                 <h2 class="font-weight-bold text-center">
-                    Travel Tickets Airways
+                    Travel Tickets
                 </h2>
                 <center>
                     <div class="row w-50">
                         <div class="col">
                             <a href="travel.php">
-                                <i class="fas fa-plane-departure h1 text-primary"></i> <br />
+                                <i class="fas fa-plane-departure h1 text-secondary"></i> <br />
                                 Flight Tickets
                             </a>
                         </div>
                         <div class="col">
                             <a href="travelrail.php">
-                                <i class="fas fa-train h1 text-secondary"></i><br />
+                                <i class="fas fa-train h1 text-primary"></i><br />
                                 Rail Tickets
                             </a>
                         </div>
@@ -107,18 +111,7 @@
                 </center>
 
                 <section class="mt-2">
-                    <div class="row">
-                        <div class="col">
-                            <button class="btn btn-primary btn-block">
-                                Domestic
-                            </button>
-                        </div>
-                        <div class="col">
-                            <button class="btn btn-outline-primary btn-block">
-                                International
-                            </button>
-                        </div>
-                    </div>
+                    
                     <div class="row mt-2 mb-2">
                         <div class="col">
                             <h6>From</h6>
@@ -204,87 +197,141 @@
                         </div>
                     </div>
                     <br />
+
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-xl">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Seat Availiblity</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="splide">
+                                    <div class="splide__track">
+                                        <ul class="splide__list">
+                                            <li class="splide__slide mr-2">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title text-success">Second Sitting (25)</h5>
+                                                        <strong>AVAILABLE-40</strong>
+                                                        <button class="btn btn-primary btn-sm btn-block">Request to Book</button>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li class="splide__slide mr-2">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title text-success">Second Sitting (25)</h5>
+                                                        <strong>AVAILABLE-40</strong>
+                                                        <button class="btn btn-primary btn-sm btn-block">Request to Book</button>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li class="splide__slide mr-2">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title text-success">Second Sitting (25)</h5>
+                                                        <strong>AVAILABLE-40</strong>
+                                                        <button class="btn btn-primary btn-sm btn-block">Request to Book</button>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li class="splide__slide mr-2">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title text-success">Second Sitting (25)</h5>
+                                                        <strong>AVAILABLE-40</strong>
+                                                        <button class="btn btn-primary btn-sm btn-block">Request to Book</button>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li class="splide__slide mr-2">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title text-success">Second Sitting (25)</h5>
+                                                        <strong>AVAILABLE-40</strong>
+                                                        <button class="btn btn-primary btn-sm btn-block">Request to Book</button>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li class="splide__slide mr-2">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title text-success">Second Sitting (25)</h5>
+                                                        <strong>AVAILABLE-40</strong>
+                                                        <button class="btn btn-primary btn-sm btn-block">Request to Book</button>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li class="splide__slide mr-2">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title text-success">Second Sitting (25)</h5>
+                                                        <strong>AVAILABLE-40</strong>
+                                                        <button class="btn btn-primary btn-sm btn-block">Request to Book</button>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <table class="table">
                         <thead>
                             <tr>
-                                <th width="10%" scope="col">Airlines</th>
-                                <th scope="col">Take Off Time</th>
-                                <th scope="col">Landing Time</th>
-                                <th scope="col">Class</th>
-                                <th scope="col">Price</th>
+                                <th width="18%" scope="col">Train Name</th>
+                                <th scope="col">Train Number</th>
+                                <th scope="col">Departure Time</th>
+                                <th scope="col">Available Time to MSF</th>
+                                <th scope="col">Seat Availability</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>
-                                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/Vistara_logo.svg/1200px-Vistara_logo.svg.png" style="width: 90%;" />
-                                </td>
-                                <td>9:30 AM IST</td>
-                                <td>11:30 AM IST</td>
-                                <td>
-                                  <select class="form-control form-control-sm" id="exampleFormControlSelect1">
-                                        <option value="Economy">Economy</option>
-                                        <option value="First Class">First Class</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    Rs. 19,000/- 
-                                </td>
-                            </tr>
 
                             <tr>
                                 <td>
-                                    <img src="https://www.goindigo.in/content/dam/indigov2/6e-website/thmbnail.jpg" style="width: 90%;" />
+                                    Shatapdi Express
                                 </td>
+                                <td>124JSU726BS</td>
                                 <td>9:30 AM IST</td>
                                 <td>11:30 AM IST</td>
                                 <td>
-                                  <select class="form-control form-control-sm" id="exampleFormControlSelect1">
-                                        <option value="Economy">Economy</option>
-                                        <option value="First Class">First Class</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    Rs. 19,000/- 
+                                  25 <br />
+                                    <button class="btn btn-danger btn-sm" type="button" data-toggle="modal" data-target="#exampleModal">
+                                      Show Birth                                  
+                                    </button>
+                                    
                                 </td>
                             </tr>
-
+                            
                             <tr>
                                 <td>
-                                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/Vistara_logo.svg/1200px-Vistara_logo.svg.png" style="width: 90%;" />
+                                    Shatapdi Express
                                 </td>
+                                <td>124JSU726BS</td>
                                 <td>9:30 AM IST</td>
                                 <td>11:30 AM IST</td>
                                 <td>
-                                  <select class="form-control form-control-sm" id="exampleFormControlSelect1">
-                                        <option value="Economy">Economy</option>
-                                        <option value="First Class">First Class</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    Rs. 19,000/- 
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/Vistara_logo.svg/1200px-Vistara_logo.svg.png" style="width: 90%;" />
-                                </td>
-                                <td>9:30 AM IST</td>
-                                <td>11:30 AM IST</td>
-                                <td>
-                                  <select class="form-control form-control-sm" id="exampleFormControlSelect1">
-                                        <option value="Economy">Economy</option>
-                                        <option value="First Class">First Class</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    Rs. 19,000/- 
+                                  25 <br />
+                                  <button class="btn btn-danger btn-sm">
+                                    Show Birth                                  
+                                  </button>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
                 </section>
+
+                
 
 
             </div>
@@ -376,7 +423,9 @@
 <!-- loader -->
 <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
-
+<script>
+	new Splide( '.splide' ).mount();
+</script>
 <script src="js/jquery.min.js"></script>
 <script src="js/jquery-migrate-3.0.1.min.js"></script>
 <script src="js/popper.min.js"></script>
